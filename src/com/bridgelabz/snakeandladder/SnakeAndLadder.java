d805bc44fe03e4fd3db8b6e7605f53c6f59cf190
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-01-11 09:02:49 +0530

    Added: check for the option program is added

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -3,9 +3,30 @@ package com.bridgelabz.snakeandladder;
 public class SnakeAndLadder {
     static public final int Starting_Position_Of_Player = 0;
     static public final int Ending_Position_Of_Player = 100;
+    public static final int Ladder = 2;
+    public static final int Snake = 1;
+    public static final int NoPlay = 0;
+    public static int rolldie(){
+        int dievalue =(int)Math.floor(Math.random()*10)%6+1;
+        return dievalue;
+    }
     public static void main(String[] args) {
         System.out.println("welcome message added");
-        int die_Roll=(int)Math.floor((Math.random()*10)%7);
-        System.out.println(die_Roll);
+
+    int rolldie = rolldie();
+    int currentPosition = 0;
+    int checkPosition= (int)Math.floor(Math.random()*10)%3;
+        switch (checkPosition){
+        case Snake:
+            currentPosition = rolldie - Snake;
+            System.out.println("player is on Snake " +checkPosition);
+            break;
+        case  Ladder:
+            currentPosition = Ladder + rolldie;
+            System.out.println("player is on Ladder " +currentPosition);
+            break;
+        default:
+            System.out.println("player is not playing " );
+        }
     }
 }
